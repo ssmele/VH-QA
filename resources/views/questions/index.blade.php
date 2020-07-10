c434ee35848963a793121b9418f2bb61bf06418b
ssmele <ssmele1995@gmail.com>
2020-07-10 17:31:33 -0600

    Removed some commented code out. Also I keep writing my commits int he vagrant box and it overrides my user profile for git. Need to stop doing that.

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -19,35 +19,3 @@
         @endforeach
     </div>
 @endsection
-
-
-{{--<div class="card">--}}
-{{--    <div class="card-header border border-dark" id="questionHeader{{$q->id}}">--}}
-{{--        <h5 class="mb-0">--}}
-{{--            <button class="btn btn-link collapsed" data-toggle="collapse"--}}
-{{--                    data-target="#questionCollapse{{$q->id}}" aria-expanded="true"--}}
-{{--                    aria-controls="questionCollapse{{$q->id}}">--}}
-{{--                {{$q->question}}--}}
-{{--                <span class="badge badge-primary badge-pill">{{count($q->answers)}}</span>--}}
-{{--            </button>--}}
-{{--        </h5>--}}
-{{--    </div>--}}
-{{--    <div id="questionCollapse{{$q->id}}" class="collapse" aria-labelledby="questionHeader{{$q->id}}"--}}
-{{--         data-parent="#questionAccordion">--}}
-{{--        <div class="card-body">--}}
-{{--            @include('questions.answers.create', ['question'=>$q])--}}
-{{--            <div class="card" id="answersListCard">--}}
-{{--                <div class="card-header">--}}
-{{--                    Submitted Answers: {{ count($q->answers) ? "" : "(No answers for this question yet! Be the first!)" }}--}}
-{{--                </div>--}}
-{{--                <ul class="list-group list-group-flush">--}}
-{{--                    @foreach($q->answers->sortBy('created_at') as $a)--}}
-{{--                        <li class="list-group-item">--}}
-{{--                            {{$a->answer}} - {{$a->created_at}}--}}
-{{--                        </li>--}}
-{{--                    @endforeach--}}
-{{--                </ul>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    </div>--}}
-{{--</div>--}}
